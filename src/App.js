aeeb0ac2fd3b00ce40d395a39f1f9d5b42ba3de7
Hoyos Juan <hoyosjuanmanuel1@gmail.com>
2020-09-23 17:31:45 -0300

    Estilos basico mis proyectos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 
 import { Home } from './pages/Home'
 import { AboutMe } from './pages/AboutMe'
+import { Portfolio } from './pages/Portfolio'
 
 export const App = () => {
   return (
     <div className="App"> 
       <Home />
       <AboutMe />
+      <Portfolio />
     </div>
   );
 }
---
+++ src/pages/Portfolio.js
@@ -0,0 +1,74 @@
+import React from 'react'
+
+import { InfoAboutMe } from '../components/InfoAboutMe'
+
+import '../css/Portfolio.css'
+import Banner from '../images/banner.svg'
+
+export const Portfolio = () => {
+  return (
+    <div className='page'>
+      <div className='container container-portfolio'>
+        <h2 className='subtitle'>Mis proyectos</h2>
+        <div className='recent'>
+          <div className='project-resume'>
+            <div className='portada-project-resume'>
+              <img src='https://i.pinimg.com/originals/64/29/75/642975b5a9f8b8414d5f3d4a6415fefc.jpg' alt='project' />
+            </div>
+            <h2 className='name-project-resume'>Space</h2>
+            <p className='label-project-resume'>Creado con ReactJS</p>
+          </div>
+          <div className='project-resume'>
+            <div className='portada-project-resume'>
+              <img src='https://i.pinimg.com/originals/64/29/75/642975b5a9f8b8414d5f3d4a6415fefc.jpg' alt='project' />
+            </div>
+            <h2 className='name-project-resume'>Space</h2>
+            <p className='label-project-resume'>Creado con ReactJS</p>
+          </div>
+          <div className='project-resume'>
+            <div className='portada-project-resume'>
+              <img src='https://i.pinimg.com/originals/64/29/75/642975b5a9f8b8414d5f3d4a6415fefc.jpg' alt='project' />
+            </div>
+            <h2 className='name-project-resume'>Space</h2>
+            <p className='label-project-resume'>Creado con ReactJS</p>
+          </div>
+          <div className='project-resume'>
+            <div className='portada-project-resume'>
+              <img src='https://i.pinimg.com/originals/64/29/75/642975b5a9f8b8414d5f3d4a6415fefc.jpg' alt='project' />
+            </div>
+            <h2 className='name-project-resume'>Space</h2>
+            <p className='label-project-resume'>Creado con ReactJS</p>
+          </div>
+          <div className='project-resume'>
+            <div className='portada-project-resume'>
+              <img src='https://i.pinimg.com/originals/64/29/75/642975b5a9f8b8414d5f3d4a6415fefc.jpg' alt='project' />
+            </div>
+            <h2 className='name-project-resume'>Space</h2>
+            <p className='label-project-resume'>Creado con ReactJS</p>
+          </div>
+          <div className='project-resume'>
+            <div className='portada-project-resume'>
+              <img src='https://i.pinimg.com/originals/64/29/75/642975b5a9f8b8414d5f3d4a6415fefc.jpg' alt='project' />
+            </div>
+            <h2 className='name-project-resume'>Space</h2>
+            <p className='label-project-resume'>Creado con ReactJS</p>
+          </div>
+          <div className='project-resume'>
+            <div className='portada-project-resume'>
+              <img src='https://i.pinimg.com/originals/64/29/75/642975b5a9f8b8414d5f3d4a6415fefc.jpg' alt='project' />
+            </div>
+            <h2 className='name-project-resume'>Space</h2>
+            <p className='label-project-resume'>Creado con ReactJS</p>
+          </div>
+          <div className='project-resume'>
+            <div className='portada-project-resume'>
+              <img src='https://i.pinimg.com/originals/64/29/75/642975b5a9f8b8414d5f3d4a6415fefc.jpg' alt='project' />
+            </div>
+            <h2 className='name-project-resume'>Space</h2>
+            <p className='label-project-resume'>Creado con ReactJS</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
